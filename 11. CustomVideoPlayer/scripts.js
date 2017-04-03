aa54b2ca8137dedfe844839464cafadf6dc832e4
listentomm <maxmarcususc@gmail.com>
2017-04-02 22:28:17 -0700

    added full screen functionality

--- 11. CustomVideoPlayer/scripts.js
+++ 11. CustomVideoPlayer/scripts.js
@@ -6,6 +6,7 @@ const progressBar = player.querySelector('.progress__filled');
 const toggleButton = player.querySelector('.toggle');
 const sliders = player.querySelectorAll('.player__slider');
 const skipButtons = player.querySelectorAll('[data-skip]');
+const fullscreen = player.querySelector('.fullscreen');
 
 // Build functions
 function togglePlay() {
@@ -37,6 +38,10 @@ function scrub(e) {
   video.currentTime = spot;
 }
 
+function screenToggle() {
+  video.webkitRequestFullscreen();
+}
+
 // Add event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateIcon);
@@ -55,3 +60,5 @@ progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
 progress.addEventListener('mousedown', () => mousedown = true);
 progress.addEventListener('mouseup', () => mousedown = false);
+
+fullscreen.addEventListener('click', screenToggle);
